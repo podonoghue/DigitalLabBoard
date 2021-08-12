274320c5af77734e818e613ba2709e6f89f51924
Peter O'Donoghue <1848579+podonoghue@users.noreply.github.com>
2021-08-12 16:18:11 +1000

    Software - Minor changes to bootloader

--- Code_Design/KinetisBootloaderApp/src/module.mk
+++ Code_Design/KinetisBootloaderApp/src/module.mk
@@ -1,9 +1,11 @@
+# Build list for Module
 # List source file to include from current directory
+
+SRC += BootloaderApp.cpp
 SRC += Bootloader.cpp
 SRC += BootloaderDialogue.cpp
 SRC += BootloaderDialogueSkeleton.cpp
 SRC += crc32b.cpp
-SRC += KinetisBootloader.cpp
 SRC += Version.rc
 SRC += ErrorMessages.cpp
 SRC += UsbdmSystem.cpp
